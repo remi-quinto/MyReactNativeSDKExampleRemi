07dd97fdea28d75bc9b63dd7f0226120bda5f86d
Remi Quinto <remi.quinto@gmail.com>
2024-01-17 11:19:53 +0700

    Fix export AuthComponent

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // index.js
 import HelloWorldComponent from './lib/HelloWorldComponent';
+import { AuthComponent } from './lib/AuthComponent';
 
 // eslint-disable-next-line prettier/prettier
-export { HelloWorldComponent };
+export { HelloWorldComponent, AuthComponent };
