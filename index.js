4bb2efce956c2ba15719c6ff02356ca147a180f6
Remi Quinto <remi.quinto@gmail.com>
2024-01-17 11:37:16 +0700

    Add hooks to SDK

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+/* eslint-disable prettier/prettier */
 // index.js
 import HelloWorldComponent from './lib/HelloWorldComponent';
 import { AuthComponent } from './lib/AuthComponent';
+import { useCustomHook } from './lib/Hooks';
 
 // eslint-disable-next-line prettier/prettier
-export { HelloWorldComponent, AuthComponent };
+export { HelloWorldComponent, AuthComponent, useCustomHook };
